c4d0b592922eede6132810818f1a99dc9a1b36fa
Alyssa Rosenzweig <bobbybee@users.noreply.github.com>
2015-06-09 16:14:15 -0400

    Code structure to process sprites and scripts

--- src/backend.js
+++ src/backend.js
@@ -5,8 +5,37 @@
  * HERE BE DRAGONS!
  */
 
+// process a script
+// this method actually hooks into code generation :)
+
+function processScript(context, script) {
+    console.log("Compiling script:");
+    console.log(script);
+}
+
+// processes a child (basically a sprite)
+
+function processChild(child) {
+    // attach some metadata to tag the sprite
+
+    child["86"] = {
+        type: "sprite"
+    };
+    
+    // sprites can have scripts
+    // process those seperately
+
+    if(typeof child.scripts !== 'undefined') {
+        child.scripts.forEach(function(script) {
+            processScript(child, script[2]);
+        });
+    }
+}
+
 module.exports = function(project, output) {
-    console.log(project);
+    project.children.forEach(function(child) {
+        processChild(child);
+    });
 
     console.log("Backend stub");
 }
